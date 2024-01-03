6f9c5dc0721a90410e044b0f066fa2a69fd300bf
shruti248 <shrutiunadkat5@gmail.com>
2024-01-03 15:41:54 +0530

    project Setup

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      // To remove the debug banner from top right corner (in phone - not web)
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home : const WeatherScreen(),
+    );
+  }
+}
+
