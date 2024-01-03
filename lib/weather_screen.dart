2a77a06c2a3d0b52b303babc5544c4928becff09
shruti248 <shrutiunadkat5@gmail.com>
2024-01-03 17:08:15 +0530

    Weather Forecast cards

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -81,10 +81,30 @@ class WeatherScreen extends StatelessWidget {
             // for spacing
             const SizedBox(height: 20,),
 
-            // Weather Forecast Height
-            const Placeholder(
-              fallbackHeight: 150,
+            // Weather Forecast cards
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Weather Forecast' , style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),),
             ),
+
+            const SizedBox(height: 16,),
+             const SingleChildScrollView(
+               scrollDirection: Axis.horizontal,
+               child: Row(
+                children: [
+
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                ],
+               ),
+             ),
 
             // for spacing
             const SizedBox(height: 20,),
@@ -102,3 +122,35 @@ class WeatherScreen extends StatelessWidget {
     );
   }
 }
+
+// creating a separate widget for Card -- that is repeating
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(14),
+        ),
+        child: const Column(
+          children: [
+            Text('03:00' , style: TextStyle(fontSize: 16 , fontWeight: FontWeight.bold),),
+            SizedBox(height: 8,),
+
+            Icon(Icons.cloud , size: 32,),
+            SizedBox(height: 8,),
+
+            Text('320.12' ,),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
